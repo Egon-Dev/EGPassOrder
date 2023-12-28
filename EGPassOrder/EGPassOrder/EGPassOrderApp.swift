2263e0ee4b3f04f5608e6d5bfc55c21522748bbd
changmuk.im <changmuk.im@phoenixdarts.com>
2023-12-28 15:04:09 +0900

    [no] feat: Create project

---
+++ EGPassOrder/EGPassOrder/EGPassOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGPassOrderApp.swift
+//  EGPassOrder
+//
+//  Created by changmuk.im@phoenixdarts.com on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct EGPassOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
