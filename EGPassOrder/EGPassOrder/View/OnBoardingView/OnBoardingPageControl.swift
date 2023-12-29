67282bb7f2d5d26eaf3b05fe64550566f788ccac
changmuk.im <changmuk.im@phoenixdarts.com>
2023-12-29 12:23:33 +0900

    [EGPO-1] feat: Add on-boarding view

---
+++ EGPassOrder/EGPassOrder/View/OnBoardingView/OnBoardingPageControl.swift
@@ -0,0 +1,47 @@
+//
+//  OnBoardingPageControl.swift
+//  EGPassOrder
+//
+//  Created by changmuk.im@phoenixdarts.com on 12/28/23.
+//
+
+import SwiftUI
+import UIKit
+
+struct OnBoardingPageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPageIndex: Int
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(control: self)
+    }
+
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.pageIndicatorTintColor = Color.onBoardingIndigo.uiColor
+        control.currentPageIndicatorTintColor = Color.onBoardingOrange.uiColor
+        control.numberOfPages = numberOfPages
+        control.addTarget(context.coordinator, action: #selector(Coordinator.updateCurrentPage(sender:)), for: .valueChanged)
+
+        return control
+    }
+
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPageIndex
+    }
+}
+
+extension OnBoardingPageControl {
+    class Coordinator: NSObject {
+        var control: OnBoardingPageControl
+
+        init(control: OnBoardingPageControl) {
+            self.control = control
+        }
+
+        @objc
+        func updateCurrentPage(sender: UIPageControl) {
+            control.currentPageIndex = sender.currentPage
+        }
+    }
+}
