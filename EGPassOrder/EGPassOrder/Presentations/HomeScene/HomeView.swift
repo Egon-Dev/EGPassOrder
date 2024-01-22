da304ba8c1b17604adcad606ee29989e4da35de6
changmuk.im <changmuk.im@phoenixdarts.com>
2024-01-22 12:19:23 +0900

    feat: Add FeedbackView

--- EGPassOrder/EGPassOrder/Presentations/HomeScene/HomeView.swift
+++ EGPassOrder/EGPassOrder/Presentations/HomeScene/HomeView.swift
@@ -70,6 +70,8 @@ fileprivate struct OrderByListView: View {
                 DineInCafeListView()
 
                 NewCafeListView()
+
+                FeedbackView()
             }
         }
     }
